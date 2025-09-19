3b9f8ad9639f0c3084987df66cbfda890ccb9526
farsanavs <farsana786441@gmail.com>
2025-09-19 11:13:10 +0530

    repo created, web and ios setup completed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './index.web.js',
+  entry: './index.web.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
